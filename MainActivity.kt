7d9fc147944fd1b29e38b2715cccd83db99f60bd
maksim12312314 <52280093+maksim12312314@users.noreply.github.com>
2020-01-16 11:30:01 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,50 +1,28 @@
-package com.example.myapplication
+package com.example.viewmodel
 
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.Serializable
+
+class mainActivityViewmodel : ViewModel() {
+    var myText:String = "hoho"
+}
+
 
 class MainActivity : AppCompatActivity() {
 
-    var namesList = listOf("Maxx", "dan", "ser", "vl", "lse")
-
-    var currentHero = Player()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        currentHero = savedInstanceState?.let {
-            it.getSerializable("our_data") as Player
-        }?:Player(namesList.shuffled().last())
+        val model = ViewModelProviders.of(this).get(mainActivityViewmodel::class.java)
+        mainText.text = model.myText
 
-        helloWorld.text = currentHero.name
-
-        Hellobutton.setOnClickListener{
-            val intent = Intent(this, Main22Activity::class.java ).apply {
-                putExtra("name", currentHero.name)
-                putExtra("health", currentHero.health)
-                putExtra("lvl", currentHero.exp)
-                putExtra("strength", currentHero.strength)
-            }
-            startActivity(intent)
+        mainButton.setOnClickListener {
+            model.myText = "Die"
+            mainText.text = model.myText
         }
-
     }
-
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putSerializable("our_data", currentHero )
-
-    }
-
-}
-
-data class Player (var name:String = "Default", var health:Int = (500..8000).shuffled().last(),  var exp:Int = (0..60).shuffled().last(),  var strength:Int = (1..60).shuffled().last()):Serializable {
-
-
 }
